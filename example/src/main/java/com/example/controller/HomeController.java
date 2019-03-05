c3d6e37c91a1bc8c3bfb90feb574e19e117b30f3
F9-HNO-D021\TRUONGNV <truongnv@five9.vn>
2019-03-05 11:31:11 +0700

    done

--- example/src/main/java/com/example/controller/HomeController.java
+++ example/src/main/java/com/example/controller/HomeController.java
@@ -1,4 +1,16 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
 }
